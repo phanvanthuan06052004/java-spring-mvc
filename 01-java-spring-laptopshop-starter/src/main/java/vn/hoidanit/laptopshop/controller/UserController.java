7ee85b1510027e0c6846f778c508dbaafe224185
phanvanthuan06052004 <phanthuan652004@gmail.com>
2024-10-18 00:33:40 +0700

    video 60 handle repo + service

--- 01-java-spring-laptopshop-starter/src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ 01-java-spring-laptopshop-starter/src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -6,11 +6,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import vn.hoidanit.laptopshop.domain.User;
+import vn.hoidanit.laptopshop.service.UserService;
 
 import org.springframework.ui.Model;
 
 @Controller
-public class HelloController {
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
     @RequestMapping("/")
     public String getHomePage(Model model) {
         model.addAttribute("data", "thuan hhhhhh");
@@ -26,6 +33,7 @@ public class HelloController {
     @RequestMapping(value = "/admin/user/create1", method = RequestMethod.POST)
     public String createUserPage(Model model, @ModelAttribute("newUser") User thuan) {
         System.out.println("hello " + thuan.toString());
+        this.userService.handleSaveUser(thuan);
         return "hello";
     }
 }
